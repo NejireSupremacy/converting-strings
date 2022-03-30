778c37de7c98afad93578351774167773fd15ce6
NejireSupremacy <JosueChan2006@outlook.com>
2022-03-29 22:12:09 -0600

    Performance improvement in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const typeFunctions = {
+    cheemssify: require('./functions/Cheemssify'),
+    emojify: require('./functions/Emojify'),
+    invertwords: require('./functions/InvertWords'),
+    isify: require('./functions/Isify'),
+    randomizeletters: require('./functions/RandomizeLetters'),
+    randomizewords: require('./functions/RandomizeWords'),
+    translatablevillagersify: require('./functions/TranslatableVillagersify'),
+    translatevillagersify: require('./functions/TranslateVillagersify'),
+    uwufy: require('./functions/Uwufy'),
+    villagersify: require('./functions/Villagersify')
+};
+
 const convertString = (str, type) => {
     if(!str) throw new Error(`You are not defining the text to convert.`);
     if(!type) throw new Error(`You are not defining the type of conversion.`);
@@ -5,12 +18,9 @@ const convertString = (str, type) => {
     if(typeof(str) !== 'string') throw new Error(`The text you are defining is not a string.`);
     if(typeof(type) !== 'string') throw new Error(`The type of conversion you are defining is not a string.`);
 
-    let func;
-    try {
-        func = require(`./functions/${type}`);
-    } catch(err) {
-        throw new Error(`The type of conversion you are defining is not valid.`) 
-    };
+    const func = typeFunctions[type.toLowerCase()];
+    if(!func) throw new Error(`The type of conversion you are defining is not valid.`);
+    
     return func(str);
 };
 
